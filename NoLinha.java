f70e51ea01870757f5cc98b6be32a47e8f85ad59
Henrique Ferreira Marciano <henrique.marciano271@gmail.com>
2025-11-11 16:41:25 -0300

    Refactor NoLinha class documentation
    Removed unnecessary comments and updated class documentation.

--- NoLinha.java
+++ NoLinha.java
@@ -1,19 +1,12 @@
 //Enrique Cipolla Martins - 10427834
 //Henrique Ferreira Marciano - 10439797
 
-/**
- * Representa um nó da lista encadeada, contendo uma linha de código.
- */
+//Representa um nó da lista encadeada, contendo uma linha de código.
 public class NoLinha {
     int numeroLinha;
     String instrucao;
     NoLinha proximo;
 
-    /**
-     * Construtor do Nó.
-     * @param numeroLinha O número da linha.
-     * @param instrucao A instrução de Assembly.
-     */
     public NoLinha(int numeroLinha, String instrucao) {
         this.numeroLinha = numeroLinha;
         this.instrucao = instrucao;
