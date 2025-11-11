f7d14a10e2d7a1fc17dfd92cb555523ced748688
Henrique Ferreira Marciano <henrique.marciano271@gmail.com>
2025-11-11 16:14:40 -0300

    Add documentation to NoLinha class and constructor
    Added documentation comments for class and constructor.

--- NoLinha.java
+++ NoLinha.java
@@ -1,14 +1,22 @@
-// Identificação do grupo (Nome e RA de cada integrante) aqui 
+//Enrique Cipolla Martins - 1042
+//Henrique Ferreira Marciano - 10439797
 
+/**
+ * Representa um nó da lista encadeada, contendo uma linha de código.
+ */
 public class NoLinha {
     int numeroLinha;
     String instrucao;
     NoLinha proximo;
 
-    // Construtor
+    /**
+     * Construtor do Nó.
+     * @param numeroLinha O número da linha.
+     * @param instrucao A instrução de Assembly.
+     */
     public NoLinha(int numeroLinha, String instrucao) {
         this.numeroLinha = numeroLinha;
         this.instrucao = instrucao;
-        this.proximo = null; // O próximo é nulo por padrão
+        this.proximo = null;
     }
 }
